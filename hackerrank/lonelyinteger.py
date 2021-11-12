53e6cb7c2e0af54660129ae5e501a6ab733a225d
rgarambha <rushabg1@umbc.edu>
2021-11-12 00:17:10 -0500

    hackerrank: lonelyinteger

---
+++ hackerrank/lonelyinteger.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    arr = []
+    for i in a:
+        if i in arr:
+            arr.remove(i)
+        else:
+            arr.append(i)
+    return arr[0]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
